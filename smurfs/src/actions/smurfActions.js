727f0a26aeeedefea1a1ad871688d6c882e8dabd
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-06-28 19:31:12 -0400

    creating smurfActions

---
+++ smurfs/src/actions/smurfActions.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const FETCH_SMURF_FAILURE = 
+'FETCH_SMURF_FAILURE';
+
+export const fetchSmurf = () => {
+    return dispatch => {
+        dispatch({ type: 'FETCH_SMURF_START'}) ;
+        axios
+            .get('http://localhost:3333/smurfs')
+            .then(res => {
+                dispatch({ type: 'FETCH_SMURF_SUCCESS',
+                payload: res.map(smurfData => )},
+                console.log("Smurf data retrieved"))
+            })
+            .catch(err => {
+                dispatch({
+                    type: 'FETCH_SMURF_FAILURE',
+                    payload: `Error ${err.response.status} : ${err.response}`
+                })
+            })
+    }
+}
